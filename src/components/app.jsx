aef8dfd6c94f7bd39cd062d145f9536f1214e38f
Joe Walker <jpdwalker@gmail.com>
2019-06-28 17:11:53 +0800

    removed flats from initial constuctor for app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -9,7 +9,7 @@ class App extends Component {
 
     this.state = {
       selectedFlat: flats[0]
-      flats
+      // flats
     };
   }
 
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -18,7 +18,7 @@ const FlatList = (props) => {
 
   return (
     <div className="flat-list">
-    {props.flats.map(flat => <Flat />)}
+   {renderList()}
     </div>
   );
 };
